b4a5dff81c611143c7b57af576e966e2484a30ca
saraok <saraok@metropolia.fi>
2023-03-05 19:11:34 +0200

    highlight

--- src/index.js
+++ src/index.js
@@ -9,10 +9,9 @@ const initializeNewsPage = async () => {
   const newsKotimaaHeadings =
     newsKotimaa.teletext.page.subpage[0].content[0].line;
 
-  console.log(newsKotimaaHeadings);
-
   const newsPageNumbers = [];
   const newsPageHeadings = [];
+  let headingContainer = [];
 
   //Creates arrays with pages numbers and headings
   for (let i = 5; i < newsKotimaaHeadings.length - 2; i++) {
@@ -26,43 +25,57 @@ const initializeNewsPage = async () => {
       );
     }
   }
-  console.log(newsPageNumbers);
-  console.log(newsPageHeadings);
 
   //creates headings to the left column
   const newsSummary = document.createElement('div');
-  newsSummary.classList = ('containerCenter');
+  newsSummary.classList = 'containerCenter';
 
   newsPageHeadings.forEach((heading) => {
     const newHeadingText = document.createElement('h3');
-    newHeadingText.id = 'headingLines';
+    newHeadingText.classList.add('headingLines');
     newHeadingText.innerHTML = heading;
     newsSummary.appendChild(newHeadingText);
   });
 
   //creates articles to the right column
-  //should be inside setInterval()
-
-  const articleArray = await getNews(105);
-  const article = articleArray.teletext.page.subpage[0].content[0].line;
-  const articleClean = [];
-
-  //Picks article lines from the array
-  for (let i = 0; i < article.length; i++) {
-    if (!(article[i].Text == undefined)) {
-      articleClean.push(article[i].Text);
-    }
-  }
-
-  //Creates the article to the right column
   const articleContainer = document.createElement('div');
-  articleContainer.classList = ('containerCenter');
-  articleClean.forEach((line) => {
-    const newsArticleLine = document.createElement('p');
-    newsArticleLine.id = 'articleLines';
-    newsArticleLine.innerHTML = line;
-    articleContainer.appendChild(newsArticleLine);
-  });
+  let numberi = 0;
+
+  const changeArticle = async () => {
+    if (numberi == newsPageNumbers.length) {
+      numberi = 0;
+    }
+
+    const articleArray = await getNews(newsPageNumbers[numberi]);
+    const article = articleArray.teletext.page.subpage[0].content[0].line;
+    const articleClean = [];
+
+    //Picks article lines from the array
+    for (let i = 0; i < article.length; i++) {
+      if (!(article[i].Text == undefined)) {
+        articleClean.push(article[i].Text);
+      }
+    }
+
+    //Creates the article to the right column
+    articleContainer.classList = 'containerCenter';
+    articleContainer.innerHTML = '';
+    articleClean.forEach((line) => {
+      const newsArticleLine = document.createElement('p');
+      newsArticleLine.id = 'articleLines';
+      newsArticleLine.innerHTML = line;
+      articleContainer.appendChild(newsArticleLine);
+    });
+    headingContainer = document.querySelectorAll('.headingLines');
+    console.log(headingContainer);
+    headingContainer.forEach((heading) => {
+      heading.style.color = 'black';
+    });
+    headingContainer[numberi].style.color = '#00b4c8';
+    numberi++;
+  };
+
+  changeArticle();
 
   //Creating body and elements
   const body = document.body;
@@ -99,6 +112,8 @@ const initializeNewsPage = async () => {
   body.appendChild(background);
   body.appendChild(heading);
   body.appendChild(main);
+
+  setInterval(await changeArticle, 10000);
 };
 
 initializeNewsPage();
